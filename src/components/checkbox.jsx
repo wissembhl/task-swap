b1179f9498374e3a64c35dde488bf777d39e7424
Wissem Bouhlel <wissem.bouhlel@ensi-uma.tn>
2023-11-03 19:25:57 +0100

    code formatting

--- src/components/checkbox.jsx
+++ src/components/checkbox.jsx
@@ -1,33 +1,41 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 export default function CheckboxesGroup({ itemsList, setItemsList }) {
-    const handleChange = (event) => {
-        const modifiedList = itemsList.map(obj => {
-            if (obj.label === event.target.name) {
-                return { ...obj, checked: event.target.checked };
-            }
-            return obj;
-        });
-        setItemsList(modifiedList)
-    };
+  const handleChange = (event) => {
+    const modifiedList = itemsList.map((obj) => {
+      if (obj.label === event.target.name) {
+        return { ...obj, checked: event.target.checked };
+      }
+      return obj;
+    });
+    setItemsList(modifiedList);
+  };
 
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
-                <FormGroup>
-                    {itemsList.sort((a , b) => a.priority < b.priority ? -1 : 1).map(ele =>
-                        <FormControlLabel
-                            control={<Checkbox checked={ele.checked} onChange={handleChange} name={ele.label} />}
-                            label={ele.label}
-                        />)}
-                </FormGroup>
-            </FormControl>
-        </Box>
-    );
+  return (
+    <Box sx={{ display: "flex" }}>
+      <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+        <FormGroup>
+          {itemsList
+            .sort((a, b) => (a.priority < b.priority ? -1 : 1))
+            .map((ele) => (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={ele.checked}
+                    onChange={handleChange}
+                    name={ele.label}
+                  />
+                }
+                label={ele.label}
+              />
+            ))}
+        </FormGroup>
+      </FormControl>
+    </Box>
+  );
 }
--- src/components/mainDasboard.jsx
+++ src/components/mainDasboard.jsx
@@ -6,75 +6,72 @@ import { MoveButton } from "./shared/moveButton";
 import CheckboxesGroup from "./checkbox";
 
 export default function MainDashboard() {
-    const [firstList, setFirstList] = useState([
-        { priority: 1, label: "1", checked: false },
-        { priority: 2, label: "2", checked: false },
-        { priority: 3, label: "3", checked: false },
-        { priority: 4, label: "4", checked: false },
-      ]);
-      const [secondList, setSecondList] = useState([
-        { priority: 5, label: "5", checked: false },
-        { priority: 6, label: "6", checked: false },
-        { priority: 7, label: "7", checked: false },
-        { priority: 8, label: "8", checked: false },
-      ]);
-    
-      /**
-       * Move items from the left box to the right one
-       */
-      const handleLeftMove = () => {
-        const modifiedList = firstList.filter((obj) => !obj.checked);
-        const checkedObject = firstList
-          .filter((obj) => obj.checked)
-          .map((ele) => {
-            return { ...ele, checked: false };
-          });
-        setFirstList(modifiedList);
-        setSecondList((prev) => [...prev, ...checkedObject]);
-      };
-    
-      /**
-       * Move items from the right box to the left one
-       */
-      const handleRightMove = () => {
-        const modifiedList = secondList.filter((obj) => !obj.checked);
-        const checkedObject = secondList
-          .filter((obj) => obj.checked)
-          .map((ele) => {
-            return { ...ele, checked: false };
-          });
-        setSecondList(modifiedList);
-        setFirstList((prev) => [...prev, ...checkedObject]);
-      };
+  const [firstList, setFirstList] = useState([
+    { priority: 1, label: "1", checked: false },
+    { priority: 2, label: "2", checked: false },
+    { priority: 3, label: "3", checked: false },
+    { priority: 4, label: "4", checked: false },
+  ]);
+  const [secondList, setSecondList] = useState([
+    { priority: 5, label: "5", checked: false },
+    { priority: 6, label: "6", checked: false },
+    { priority: 7, label: "7", checked: false },
+    { priority: 8, label: "8", checked: false },
+  ]);
 
-    return (
-        <Stack
-        spacing={2}
-        direction="row"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-evenly",
-          height: "100vh",
-        }}
-      >
-        <Box sx={{ borderRadius: 1, border: 1, padding: 10 }}>
-          <CheckboxesGroup itemsList={firstList} setItemsList={setFirstList} />
-        </Box>
-        <Stack direction="column" spacing={2}>
-          <MoveButton variant="contained" onClick={handleLeftMove}>
-            {">"}
-          </MoveButton>
-          <MoveButton variant="contained" onClick={handleRightMove}>
-            {"<"}
-          </MoveButton>
-        </Stack>
-        <Box sx={{ borderRadius: 1, border: 1, padding: 10 }}>
-          <CheckboxesGroup
-            itemsList={secondList}
-            setItemsList={setSecondList}
-          />
-        </Box>
+  /**
+   * Move items from the left box to the right one
+   */
+  const handleLeftMove = () => {
+    const modifiedList = firstList.filter((obj) => !obj.checked);
+    const checkedObject = firstList
+      .filter((obj) => obj.checked)
+      .map((ele) => {
+        return { ...ele, checked: false };
+      });
+    setFirstList(modifiedList);
+    setSecondList((prev) => [...prev, ...checkedObject]);
+  };
+
+  /**
+   * Move items from the right box to the left one
+   */
+  const handleRightMove = () => {
+    const modifiedList = secondList.filter((obj) => !obj.checked);
+    const checkedObject = secondList
+      .filter((obj) => obj.checked)
+      .map((ele) => {
+        return { ...ele, checked: false };
+      });
+    setSecondList(modifiedList);
+    setFirstList((prev) => [...prev, ...checkedObject]);
+  };
+
+  return (
+    <Stack
+      spacing={2}
+      direction="row"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        height: "100vh",
+      }}
+    >
+      <Box sx={{ borderRadius: 1, border: 1, padding: 10 }}>
+        <CheckboxesGroup itemsList={firstList} setItemsList={setFirstList} />
+      </Box>
+      <Stack direction="column" spacing={2}>
+        <MoveButton variant="contained" onClick={handleLeftMove}>
+          {">"}
+        </MoveButton>
+        <MoveButton variant="contained" onClick={handleRightMove}>
+          {"<"}
+        </MoveButton>
       </Stack>
-    );
+      <Box sx={{ borderRadius: 1, border: 1, padding: 10 }}>
+        <CheckboxesGroup itemsList={secondList} setItemsList={setSecondList} />
+      </Box>
+    </Stack>
+  );
 }
